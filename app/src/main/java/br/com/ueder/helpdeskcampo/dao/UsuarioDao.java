89edd2b40d8d394081903f6ba9f6d891543265a9
Ueder <uedercosta@gmail.com>
2017-12-12 22:15:00 -0200

    Primeiro Commit

--- app/src/main/java/br/com/ueder/helpdeskcampo/dao/UsuarioDao.java
+++ app/src/main/java/br/com/ueder/helpdeskcampo/dao/UsuarioDao.java
@@ -1,7 +1,9 @@
 package br.com.ueder.helpdeskcampo.dao;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
+import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 
 import br.com.ueder.helpdeskcampo.modelo.Usuario;
@@ -29,12 +31,40 @@ public class UsuarioDao {
 
     public Usuario getUsuarioByLoginSenha(String login, String senha){
         String[] args = {login, senha};
-        Cursor cursor = db.rawQuery("SELECT * FROM USUARIO WHERE LOGIN = ? AND SENHA = ?", args);
+        Cursor cursor = db.rawQuery("SELECT * FROM USUARIOS WHERE LOGIN = ? AND SENHA = ?", args);
         cursor.moveToNext();
         Integer id = cursor.getInt(cursor.getColumnIndex("ID_USUARIO"));
         String password = cursor.getString(cursor.getColumnIndex("SENHA"));
         Integer admin = cursor.getInt(cursor.getColumnIndex("MASTER"));
         Usuario usuario = new Usuario(id, password, admin);
         return usuario;
+    }
+
+    public void salvar(Usuario usuario){
+        db.beginTransaction();
+        try {
+            ContentValues cv = new ContentValues();
+            cv.put("ID_USUARIO", usuario.getId());
+            cv.put("SENHA", usuario.getSenha());
+            cv.put("MASTER", usuario.getAdmin());
+            db.insert("USUARIOS", null, cv);
+            db.setTransactionSuccessful();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            db.endTransaction();
+        }
+    }
+
+    public void excluir(){
+        db.beginTransaction();
+        try {
+            db.execSQL("DELETE FROM USUARIOS");
+            db.setTransactionSuccessful();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            db.endTransaction();
+        }
     }
 }
